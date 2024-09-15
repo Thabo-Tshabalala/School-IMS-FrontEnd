a248ce2d5b1d88ff11c1da22872ae11e3ba0b474
Thabo Tshabalala <justicethabo21@gmail.com>
2024-09-15 03:23:50 +0200

    setup

--- school-ims/src/app/app.component.ts
+++ school-ims/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'School-Manangement-Inventory-System';
+  title = 'SchoolManangement-Inventory-System';
 }
