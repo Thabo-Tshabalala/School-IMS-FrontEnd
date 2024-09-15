5398c35fbc547d985ce481f2af390d04b2a699bb
Thabo Tshabalala <justicethabo21@gmail.com>
2024-09-15 03:42:55 +0200

    Navigatin

--- school-ims/src/app/app.routes.ts
+++ school-ims/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: NavigationComponent },  // Default route
+  // Add additional routes as needed
+];
